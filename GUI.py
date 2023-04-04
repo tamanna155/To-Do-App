841c3a7ef56ca8e36f10d08604bc4e16f50f2c4e
tamanna@155 <tamannatripathy1@gmail.com>
2023-04-04 16:20:37 +0530

    Added a add function to the GUI

--- GUI.py
+++ GUI.py
@@ -5,6 +5,14 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
 window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]], font=('Kanit, sans-serif', 16))
-event = window.read()
-print(event)
+while True:
+    event, value = window.read()
+    print(event)
+    print(value)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = value['todo'] + "\n"
+            todos.append(new_todos)
+            functions.write_todos("todos.txt", todos)
 window.close()
