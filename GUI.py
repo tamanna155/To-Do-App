28772c4695b4b05fb077a2eb552eacfcb42e08c0
tamanna@155 <tamannatripathy1@gmail.com>
2023-04-05 19:22:09 +0530

    Edited themes and some colours of GUI

--- GUI.py
+++ GUI.py
@@ -1,15 +1,18 @@
 import functions
 import PySimpleGUI as sg
-
+import time
+sg.theme("Black")
+clock = sg.Text(" ", key="watch")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="items", enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button]], font=('Kanit, sans-serif', 16))
+window = sg.Window('My To-Do App', layout=[[clock], [label], [input_box, add_button], [list_box, edit_button, complete_button]], font=('Kanit, sans-serif', 16))
 while True:
-    event, value = window.read()
+    event, value = window.read(timeout=200)
+    window['watch'].update(value=time.strftime("%B %d, %Y %H:%M:%S"))
     print(event)
     print(value)
     match event:
@@ -21,13 +24,16 @@ while True:
             window['items'].update(values=todos)
 
         case "Edit":
-            item = value['items'][0]
-            new_item = value['todo'] + "\n"
-            todos = functions.get_todos()
-            index = todos.index(item)
-            todos[index] = new_item
-            functions.write_todos("todos.txt", todos)
-            window['items'].update(values=todos)
+            try:
+                item = value['items'][0]
+                new_item = value['todo'] + "\n"
+                todos = functions.get_todos()
+                index = todos.index(item)
+                todos[index] = new_item
+                functions.write_todos("todos.txt", todos)
+                window['items'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first", font=('Kanit', 16), button_color='red')
 
         case "Complete":
             comp = value['items'][0]
