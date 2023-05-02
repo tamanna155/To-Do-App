44634bb10c3f0cbba3a3bd470033bb8c729864a6
tamanna@155 <tamannatripathy1@gmail.com>
2023-05-02 22:25:41 +0530

    Implemented Add new todo to the WebApp

--- GUI.py
+++ GUI.py
@@ -1,12 +1,17 @@
 import functions
 import PySimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'r') as file:
+        pass
 sg.theme("Black")
 clock = sg.Text(" ", key="watch")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
-add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.get_todos(), key="items", enable_events=True, size=[45,10])
+add_button = sg.Button("Add", tooltip="Add", size=10, mouseover_colors="LightBlue")
+list_box = sg.Listbox(values=functions.get_todos(), key="items", enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 window = sg.Window('My To-Do App', layout=[[clock], [label], [input_box, add_button], [list_box, edit_button, complete_button]], font=('Kanit, sans-serif', 16))
@@ -22,6 +27,7 @@ while True:
             todos.append(new_todos)
             functions.write_todos("todos.txt", todos)
             window['items'].update(values=todos)
+            window['todo'].update(value=" ")
 
         case "Edit":
             try:
@@ -41,7 +47,7 @@ while True:
             todos.remove(comp)
             functions.write_todos("todos.txt", todos)
             window['items'].update(values=todos)
-            window['todo'].update(values="")
+            window['todo'].update(value="")
 
         case "items":
             window['todo'].update(value=value['items'][0])
---
+++ WebApp.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos("todos.txt", todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity")
+for data in todos:
+    st.checkbox(data)
+st.text_input(label='Enter a Todo', placeholder='Add a new todooooo', on_change=add_todo, key='new_todo')
